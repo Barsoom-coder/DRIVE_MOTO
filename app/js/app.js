ef49706389f1176784fba0293a8e549034642c20
roman_kononov <romankononovdev@mail.ru>
2020-12-15 08:58:43 +0700

    added second section products

--- app/js/app.js
+++ app/js/app.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	$('.tab').on('click', function (e) { 
 		e.preventDefault();
 
-		$('.tab').removeClass('tab--active');
-		$('.tabs-content').removeClass('tabs-content--active');
+		$($(this).siblings()).removeClass('tab--active');
+		$($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
 
 		$(this).addClass('tab--active');
 		$($(this).attr('href')).addClass('tabs-content--active');
