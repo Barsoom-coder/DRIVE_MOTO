5ef330012b1c408eb83de4fa3e1df1399b8f198b
roman_kononov <romankononovdev@mail.ru>
2020-12-21 10:37:19 +0700

    added responsiveness to product-page.html page

--- app/js/app.js
+++ app/js/app.js
@@ -23,9 +23,11 @@ document.addEventListener("DOMContentLoaded", () => {
   $(".tab").on("click", function (e) {
     e.preventDefault();
     $($(this).siblings()).removeClass("tab--active");
-    $($(this).parent().siblings().find("div")).removeClass("tabs-content--active");
+    $($(this).closest('.tabs-wrapper').siblings().find("div")).removeClass("tabs-content--active");
     $(this).addClass("tab--active");
     $($(this).attr("href")).addClass("tabs-content--active");
+
+    $('.product-slider').slick('setPosition').
   });
   $(".product-item__favorite").on("click", function (e) {
     $(this).toggleClass("product-item__favorite--active");
